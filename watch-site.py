0e97379e0fe41ecbe7e4d48d8ffec46abeac2111
Ваше Имя <@commeta>
2020-03-25 11:25:31 +0300

    commit watch-site.py

--- watch-site.py
+++ watch-site.py
@@ -21,7 +21,8 @@
 #  MA 02110-1301, USA.
 #  
 #  Insert your token, and channel id - from telegram API
-#  And change site domain for watch, example site.com
+#  And change site domain for watch, example: site.com
+#  Install tor, or any socks proxy server, example: localhost:9100 
 
 import socks
 import socket
